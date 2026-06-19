dad6a936e65e4717e18794063336430d000539f3
ImportFromCBastian <sebshndz2001@gmail.com>
2026-06-19 18:47:57 -0300

    hotfix

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import About from '@/components/about'
-import Contact from '@/components/contact'
-import Hero from '@/components/hero'
-import ProjectList from '@/components/projects/project-list'
-import SkillsList from '@/components/skills/skill-list'
+import About from './components/about'
+import Contact from './components/contact'
+import Hero from './components/hero'
+import ProjectList from './components/projects/project-list'
+import SkillsList from './components/skills/skill-list'
 
 export default function Home() {
   return (
