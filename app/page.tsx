c3075aa4ba0fb825c77258cee4a147e713cf9e19
ImportFromCBastian <sebshndz2001@gmail.com>
2026-06-19 17:42:54 -0300

    refactor

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 import About from '@/components/about'
 import Contact from '@/components/contact'
 import Hero from '@/components/hero'
-import Project from '@/components/projects/project-list'
-import Skills from '@/components/skills/skill-list'
+import ProjectList from '@/components/projects/project-list'
+import SkillsList from '@/components/skills/skill-list'
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <About />
-      <Project />
-      <Skills />
+      <ProjectList />
+      <SkillsList />
       <Contact />
     </main>
   )
